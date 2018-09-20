531dd691fa9fb8d9fa11bfd33a26048b55a38941
Kyle Knott <kyleknott11111@gmail.com>
2018-09-19 23:25:25 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="count-section">
+      # of shuffles: {{ shuffleCount }}
+    </div>
+    <h1 class="title">
+      <img class="vue-logo" src="./assets/logo.png" />
+      Card Shuffling
+    </h1>
+    <div class="speed-buttons">
+      <button v-for="type in shuffleTypes"
+        class="button is-small"
+        :class="{ 'is-light': shuffleSpeed !== `shuffle${type}` }"
+        @click="shuffleSpeed = `shuffle${type}`">
+        {{ type }}
+      </button>
+    </div>
+    <div class="main-buttons">
+      <button v-if="isDeckShuffled"
+        class="button is-primary is-outlined"
+        @click="displayInitialDeck">
+        Reset <i class="fas fa-undo"></i>
+      </button>
+      <button @click="shuffleDeck" class="button is-primary">
+        Shuffle <i class="fas fa-random"></i>
+      </button>
+    </div>
+    <transition-group :name="shuffleSpeed" tag="div" class="deck">
+      <Card
+        v-for="card in cards"
+        :key="card.id"
+        :card="card"
+      />
+    </transition-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Card from './components/Card'
+  import shuffle from 'lodash/shuffle'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      Card
+    },
+    data () {
+      return {
+        ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+        suits: ['♥','♦','♠','♣'],
+        cards: [],
+        shuffleSpeed: 'shuffleMedium',
+        shuffleTypes: ['Slow', 'Medium', 'Fast'],
+        isDeckShuffled: false,
+        shuffleCount: 0
+      }
+    },
+    created () {
+      this.displayInitialDeck()
+    },
+    methods: {
+      displayInitialDeck () {
+        const cardsWithoutIds = [].concat(
+          ...this.suits.map(suit => this.ranks.map(rank => ({ suit, rank })))
+        )
+
+        this.cards = cardsWithoutIds.map((card, index) => ({ ...card, id: index + 1 }))
+        this.isDeckShuffled = false
+        this.shuffleCount = 0
+      },
+      shuffleDeck () {
+        this.cards = shuffle(this.cards)
+        this.isDeckShuffled = true
+        this.shuffleCount++
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./styles/app.css"></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card"
+    :class="suitColor[card.suit]">
+      <span class="card__suit card__suit--top">{{ card.suit }}</span>
+      <span class="card__number">{{ card.rank }}</span>
+      <span class="card__suit card__suit--bottom">{{ card.suit }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+    props: {
+      card: Object
+    },
+    data () {
+      return {
+        suitColor: {
+          '♠': 'black',
+          '♣': 'black',
+          '♦': 'red',
+          '♥': 'red',
+        }
+      }
+    }
+  }
+</script>
+
+<style src="../styles/card.css"></style>
